8bdbb4beb0b5df665b6ea855023a63b555ce8828
Leonardo Avendaño <leonardo.avendano@phinxlab.com>
2021-10-01 14:00:22 -0300

    Initial push

---
+++ __test__/app.test.js
@@ -0,0 +1,10 @@
+
+describe('Testing to see if Jest works', () => {
+  it('This fails...', () => {
+    expect(1).toBe(2);
+  });
+  
+  it('This passes!', () => {
+    expect(1).toBe(1);
+  });
+})
