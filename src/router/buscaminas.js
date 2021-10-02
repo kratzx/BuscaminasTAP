a62c7afbcb5718679ec5d612465a22a40065113a
Leonardo Avendaño <leo.aven94@gmail.com>
2021-10-01 23:41:59 -0300

    Add GET and POST handlers for buscaminas EP

--- src/router/buscaminas.js
+++ src/router/buscaminas.js
@@ -1,7 +1,32 @@
+import buscaminasFactory from "../service/buscaminasFactory.js";
+
 export const buscaminas = (app) => {
-  app.get('/buscaminas', rootHandler);
+  app.get('/buscaminas', newMatch);
+  app.get('/buscaminas/:id', findMatch);
+  app.post('/buscaminas', saveMatch);
 }
 
-const rootHandler = (req, res) => {
-  res.send('Buscaminas root working!');
+const newMatch = (req, res) => {
+  try {
+    res.send(buscaminasFactory.create());
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+}
+
+const findMatch = (req, res) => {
+  try {
+    res.send(buscaminasFactory.findById(req.params.id));
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+}
+
+const saveMatch = (req, res) => {
+  try {
+    buscaminasFactory.save(req.body);
+    res.send('Partida guardada con exito!');
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
 }
